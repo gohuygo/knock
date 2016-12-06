3c1b798334a7289d6bf98e1ddac5b84707ea8ad4
Huy Nguyen <huy@gohuygo.com>
2016-12-05 17:15:10 -0800

    remove rescue

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -43,12 +43,8 @@ module Knock::Authenticable
       memoization_var_name = "@_#{getter_name}"
       self.class.send(:define_method, getter_name) do
         unless instance_variable_defined?(memoization_var_name)
-          current =
-            begin
-              Knock::AuthToken.new(token: token).entity_for(entity_class)
-            rescue
-              nil
-            end
+          current = Knock::AuthToken.new(token: token).entity_for(entity_class)
+
           instance_variable_set(memoization_var_name, current)
         end
         instance_variable_get(memoization_var_name)
